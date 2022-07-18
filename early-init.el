750dbbcd1c9d95cd7090a379ea353eb9938371ca
Cássio Ávila <cassioavila@protonmail.com>
2022-07-18 07:16:58 -0300

    rewrite config in org mode

---
+++ early-init.el
@@ -0,0 +1,35 @@
+;; -*- lexical-binding: t; -*-
+
+;; Garbage Collections
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+(add-hook 'emacs-startup-hook
+	  #'(lambda () (setq gc-cons-threshold (* 2 1000 1000))))
+
+;; Data emacs reads from process
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+;; Compile warnings
+;;  (setq warning-minimum-level :emergency)
+(setq native-comp-async-report-warnings-errors 'silent) ;; native-comp warning
+(setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local))
+
+(setq package-native-compile t
+      native-comp-deferred-compilation t
+      native-comp-async-report-warnings-errors nil)
+
+;; MISC OPTIMIZATIONS ----
+  ;;; optimizations (froom Doom's core.el). See that file for descriptions.
+(setq idle-update-delay 1.0)
+
+;; Disabling bidi (bidirectional editing stuff)
+(setq-default bidi-display-reordering 'left-to-right 
+	      bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)  ; emacs 27 only - disables bidirectional parenthesis
+
+(setq highlight-nonselected-windows nil)
+(setq fast-but-imprecise-scrolling t)
+(setq inhibit-compacting-font-caches t)
+
+;; Disable default package.el
+(setq package-enable-at-startup nil)
--- init.el
+++ init.el
@@ -1,16 +1,12 @@
-; Garbage collector
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
-
-(add-hook 'emacs-startup-hook
-          #'(lambda () (setq gc-cons-threshold (* 2 1000 1000))))
-
-;; Data emacs reads from process
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
-
-; Quality of life
 (setq use-short-answers t)
 (setq ring-bell-function 'ignore)
+
+;; scroll
+(setq scroll-conservatively 1000)
+(setq scroll-margin 2)
+
+;; Revert window changes
+(winner-mode)
 
 ; Cache directory
 (setq user-emacs-directory "~/.cache/emacs/")
@@ -27,28 +23,13 @@
       kept-old-versions 10
       kept-new-versions 10)
 
-(electric-pair-mode t)
-; Configuration directory
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-(when (file-exists-p custom-file)
-    (load custom-file t))
-
-; Native compilation
-(setq package-native-compile t
-      native-comp-deferred-compilation t
-      native-comp-async-report-warnings-errors nil)
-
 (setq native-comp-eln-load-path
       (list (expand-file-name "eln-cache" user-emacs-directory)))
 
-; Visual
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
-
 
 (blink-cursor-mode 0)
 
@@ -58,29 +39,28 @@
       server-client-instructions nil)
 
 (load-theme 'mplex t)
+
 ; Line number mode
 (setq display-line-numbers-type 'relative)
 
 (global-display-line-numbers-mode)
 
-; Scroll
-(setq scroll-conservatively 1000)
-(setq scroll-margin 2)
-
-(defun my-set-font-faces ()
-  (let* ((my-font "GoMono Nerd Font Mono 9")
-	 (fallback "monospace")
-	 (font (if (x-list-fonts my-font) my-font fallback)))
+(defun my/set-font-faces ()
+  (let* ((main-font "GoMono Nerd Font Mono 9")
+         (fallback "monospace")
+         (font (if (x-list-fonts main-font) main-font fallback)))
     (set-face-attribute 'default nil :font font)
     (set-face-attribute 'fixed-pitch nil :font font)))
-
-;; (my-set-font-faces)
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
               (lambda (frame)
-                (with-selected-frame frame (my-set-font-faces))))
-  (my-set-font-faces))
+                (with-selected-frame frame (my/set-font-faces))))
+  (my/set-font-faces))
+
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (electric-pair-local-mode t)))
 
 ; Straight bootstrap
 (setq straight-check-for-modifications nil)
@@ -88,7 +68,8 @@
 (defvar bootstrap-version)
 
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
@@ -102,6 +83,7 @@
 ; Straight config
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
+(require 'org-tempo)
 
 (defun my-set-evil-keybinds ()
   (evil-set-leader 'normal (kbd "SPC"))
@@ -110,26 +92,25 @@
   (evil-define-key 'normal 'global (kbd "<leader>sb") 'sp-forword-barf-sexp)
   (evil-define-key 'normal 'global (kbd "<leader>lb") 'switch-to-buffer))
 
-; Evil mode
 (use-package evil
   :demand t
   :bind (("<escape>" . keyboard-escape-quit))
   :init
   (setq evil-operator-state-tag "OPR"
-	evil-normal-state-tag "NOR"
-	evil-insert-state-tag "INS"
-	evil-visual-state-tag "VIS"
-	evil-replace-state-tag "REP"
-	evil-emacs-state-tag "EMC"
-	evil-motion-state-tag "MOT")
+        evil-normal-state-tag "NOR"
+        evil-insert-state-tag "INS"
+        evil-visual-state-tag "VIS"
+        evil-replace-state-tag "REP"
+        evil-emacs-state-tag "EMC"
+        evil-motion-state-tag "MOT")
 
   (use-package undo-fu)
 
   (setq evil-echo-state nil
-	evil-undo-system 'undo-fu
-	evil-want-C-u-scroll t
-	evil-want-Y-yank-to-eol t
-	evil-search-module 'evil-search)
+        evil-undo-system 'undo-fu
+        evil-want-C-u-scroll t
+        evil-want-Y-yank-to-eol t
+        evil-search-module 'evil-search)
 
   :custom
   (evil-want-keybinding nil)
@@ -153,7 +134,6 @@
 (use-package evil-commentary
   :init (evil-commentary-mode))
 
-; Vertico
 (use-package vertico
   :init
   (use-package savehist
@@ -167,9 +147,15 @@
   :config
   (which-key-mode))
 
-(use-package rainbow-mode
+(use-package pulsar
+  :init
+  (pulsar-global-mode))
+
+(use-package direnv
   :config
-  (rainbow-mode))
+  (direnv-mode))
+
+(use-package rainbow-mode)
 
 (use-package dashboard
   :config
@@ -179,7 +165,6 @@
 
   (dashboard-setup-startup-hook))
 
-;; Languages modes
 (use-package fish-mode)
 
 (use-package lua-mode)
@@ -191,23 +176,23 @@
 (use-package nix-mode
   :mode "\\.nix\\'")
 
-(use-package magit)
+(use-package clojure-mode)
 
 (use-package cider
   :init
   (setq cider-show-error-buffer nil))
 
 (use-package yasnippet-snippets)
+
 (use-package yasnippet
   :init
   (yas-global-mode))
 
-;; Auto completion an lsp stuff
 (use-package company
   :config
   (setq company-minimum-prefix-length 1
         company-idle-delay 0.0 
-	company-selection-wrap-around t)
+        company-selection-wrap-around t)
 
   (setq company-format-margin-function #'company-text-icons-margin)
   (setq company-tooltip-align-annotations t)
@@ -215,7 +200,7 @@
 
   ;; make company evil compatible
   (mapc #'evil-declare-change-repeat
-	'(company-complete-common
+        '(company-complete-common
           company-select-next
           company-select-previous
           company-complete-selection))
@@ -231,33 +216,43 @@
 
   :hook ((clojure-mode . lsp-deferred)
          (go-mode . lsp-deferred)
-	 (c-mode . lsp-deferred)
-	 (lsp-mode . flycheck-mode)
-	 (lsp-mode . yas-minor-mode)
+         (c-mode . lsp-deferred)
+         (lsp-mode . flycheck-mode)
+         (lsp-mode . yas-minor-mode)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp lsp-deferred)
 
 (use-package lsp-ui :commands lsp-ui-mode)
 
-(use-package pulsar
-  :init
-  (pulsar-global-mode))
-
-(use-package flycheck)
-
-(use-package direnv
-  :config
-  (direnv-mode))
+(use-package doom-themes)
 
 (use-package doom-modeline
   :init
   (setq doom-modeline-height 0)
   :hook (after-init . doom-modeline-mode))
 
+(defun my/org-mode-setup ()
+  (display-line-numbers-mode 0)
+  (org-indent-mode)
+  ;; (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+
+  (company-mode 0)
+  ;; Org tempo
+  (require 'org-tempo)
+
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python")))
+
 (use-package org
+  :hook
+  (org-mode . my/org-mode-setup)
   :config
-  (setq org-src-fontify-natively t) ;; Syntax highlighting in org src blocks
-  (setq org-highlight-latex-and-related '(native)) ;; Highlight inline LaTeX
-  (setq org-startup-folded 'show2levels) ;; Org files start up folded by default
-  (setq org-image-actual-width 300)
-  (setq org-fontify-whole-heading-line t))
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
