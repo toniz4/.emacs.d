9de32daaf9b6ac61ed6f7ac633f572412699f5f1
Cássio Ávila <cassioavila@protonmail.com>
2022-07-20 09:38:17 -0300

    some changes

--- init.el
+++ init.el
@@ -58,6 +58,13 @@
 
 (global-display-line-numbers-mode)
 
+;; Don't resize the frames in steps; it looks weird, especially in tiling window
+;; managers, where it can leave unseemly gaps.
+(setq frame-resize-pixelwise t)
+
+;; But don't resize pixelwise
+(setq window-resize-pixelwise nil)
+
 (defun my/set-font-faces ()
   (let* ((main-font "GoMono Nerd Font Mono 9")
          (fallback "monospace")
@@ -85,9 +92,34 @@
           (lambda ()
             (add-hook 'after-save-hook #'my/org-tangle-config)))
 
+(add-hook 'org-present-mode-hook
+          (lambda ()
+            (visual-fill-column-mode 1)
+            (setq mode-line-format nil)))
+
+(add-hook 'org-present-mode-quit-hook
+          (lambda ()
+            (visual-fill-column-mode 0)
+            (doom-modeline-mode)))
+
 (add-hook 'go-mode-hook
           (lambda ()
             (setq-local tab-width 4)))
+
+;; Switch to the scratch buffer
+(defun my/switch-to-scratch-buffer ()
+  (interactive)
+  (switch-to-buffer "*scratch*"))
+
+(setq bookmark-save-flag 1
+      bookmark-set-fringe-mark nil)
+
+(defun my/bookmark-make-record ()
+  `((filename . ,(buffer-file-name))))
+
+(setq bookmark-make-record-function #'my/bookmark-make-record)
+
+(save-place-mode)
 
 ; Straight bootstrap
 (setq straight-check-for-modifications nil)
@@ -112,11 +144,12 @@
 (setq straight-use-package-by-default t)
 (require 'org-tempo)
 
-(defun my-set-evil-keybinds ()
+(defun my/set-evil-keybinds ()
   (evil-set-leader 'normal (kbd "SPC"))
   (evil-define-key 'normal 'global (kbd "<leader>lf") 'find-file)
   (evil-define-key 'normal 'global (kbd "<leader>ss") 'sp-forward-slurp-sexp)
   (evil-define-key 'normal 'global (kbd "<leader>sb") 'sp-forword-barf-sexp)
+  (evil-define-key 'motion help-mode-map "q" 'kill-this-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>lb") 'switch-to-buffer))
 
 (use-package evil
@@ -142,8 +175,7 @@
   :custom
   (evil-want-keybinding nil)
   :config
-  (evil-mode 1)
-  (my-set-evil-keybinds))
+  (evil-mode 1))
 
 (use-package evil-collection
   :demand t
@@ -160,6 +192,36 @@
 
 (use-package evil-commentary
   :init (evil-commentary-mode))
+
+(use-package general
+  :init
+  (general-define-key
+   :states '(normal motion visual)
+   :keymaps 'override
+   :prefix "SPC"
+
+   "SPC" '(execute-extended-command :which-key "M-x")
+   "q" '(save-buffers-kill-emacs :which-key "quit emacs")
+   ;; Buffes 
+   "b" '(nil :which-key "buffer")
+   "ba" '(bookmark-set :which-key "set bookmark")
+   "bb" '(switch-to-buffer :which-key "switch buffers")
+   "bd" '(evil-delete-buffer :which-key "delete buffer")
+   "bk" '(kill-buffer :which-key "kill other buffers")
+   "bs" '(my/switch-to-scratch-buffer :which-key "scratch buffer")
+   "bi" '(clone-indirect-buffer  :which-key "indirect buffer")
+   "br" '(revert-buffer :which-key "revert buffer")
+
+   ;; Files
+   "f" '(nil :which-key "files")
+   "fb" '(counsel-bookmark :which-key "bookmarks")
+   "ff" '(find-file :which-key "find file")
+   ;; "fn" '(new-file :which-key "new file")
+   ;; "fr" '(counsel-recentf :which-key "recent files")
+   "fR" '(rename-file :which-key "rename file")
+   "fs" '(save-buffer :which-key "save buffer")
+   "fS" '(evil-write-all :which-key "save all buffers")
+   ))
 
 (use-package vertico
   :init
@@ -194,6 +256,13 @@
   (setq dashboard-center-content t)
 
   (dashboard-setup-startup-hook))
+
+(use-package visual-fill-column
+  :commands
+  (visual-fill-column-mode)
+  :init
+  (setq visual-fill-column-center-text t
+        visual-fill-column-width 110))
 
 (use-package fish-mode)
 
@@ -288,3 +357,6 @@
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode))
+
+(use-package org-present
+  :commands (org-present))
